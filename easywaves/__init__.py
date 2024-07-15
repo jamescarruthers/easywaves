866228f568ecb3b4ba3c6ca8535347a0ce6b1b7c
James Carruthers <james@mintylamb.co.uk>
2024-07-15 12:50:49 +0100

    Added curves using numpy

--- easywaves/__init__.py
+++ easywaves/__init__.py
@@ -1,1 +1,3 @@
-from .easywaves import *
+from .easywaves import ease, ease_map, ease_xy, wave
+from .curves import Curves, npCurves
+__all__ = ['Curves']
--- test.py
+++ test.py
@@ -1,12 +1,29 @@
-from easywaves import *
-import matplotlib.pyplot as plt
+# from easywaves import ease_map
+from easywaves import ease, ease_map, ease_xy, wave, Curves, npCurves
 from numpy import arange
+import matplotlib.pyplot as plt
+
+
+
+xy = ease_xy([0,0], [10,100])
+
+x, y = xy[:, 0], xy[:, 1]
+
+# Plot the result
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, '-x')
+plt.xlabel('Time/Distance')
+plt.ylabel('Y')
+plt.grid(True)
+plt.legend()
+plt.show()
+
 
 t = []
 y = []
 for i in range (0, 101):
     t.append(i)
-    y.append(ease_map(i, 0, 100, -10, 10, circIn))
+    y.append(ease_map(i, 0, 100, 10, -10, Curves.circIn))
 
 # Plot the result
 plt.figure(figsize=(10, 6))
@@ -21,7 +38,7 @@ t = []
 y = []
 for i in arange (0, 2, 0.01):
     t.append(i)
-    y.append(wave(i, expoInOut) * 100)
+    y.append(wave(i, Curves.sineInOut) * 100)
 
 # Plot the result
 plt.figure(figsize=(10, 6))
@@ -37,7 +54,7 @@ t = []
 y = []
 for i in arange (0, 10, 0.01):
     t.append(i)
-    y.append(decayDamped(i, 100, 10))
+    y.append(Curves.decayDamped(i, 100, 10))
 
 # Plot the result
 plt.figure(figsize=(10, 6))
